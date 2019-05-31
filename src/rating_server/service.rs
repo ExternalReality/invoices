e5f7b1ca93c1796eb6c1ec53dba0cfd7d3ee00cd
externalreality <ecjones@protonmail.com>
2019-05-31 18:19:13 -0400

    Test bill calculation.

--- src/rating_server/service.rs
+++ src/rating_server/service.rs
@@ -34,3 +34,22 @@ impl Rating for RatingService {
 fn calculate_bill(invoices: &[Invoice]) -> f64 {
     invoices.iter().fold(0.0, |acc, x| acc + x.total_price)
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::invoice::Invoice;
+
+    #[test]
+    fn add_bill_values() {
+        let mut inv1 = Invoice::new();
+        let mut inv2 = Invoice::new();
+
+        inv1.set_total_price(100.21);
+        inv2.set_total_price(230.33);
+
+        assert_eq!(
+            super::calculate_bill(&[inv1.clone(), inv2.clone()]),
+            inv1.total_price + inv2.total_price
+        )
+    }
+}
