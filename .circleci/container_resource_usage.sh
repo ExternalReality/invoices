ab3f5e93e2ddd242fdd2bb15d8fb8856ae2d3f7a
externalreality <ejones@protonmail.com>
2020-06-02 10:53:01 -0400

    Move sample script into command.

--- .circleci/container_resource_usage.sh
+++ .circleci/container_resource_usage.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
+OD#!/bin/bash
 
 set -e
 
-tstart=$(date +%s%N)
-cstart=$(cat /sys/fs/cgroup/cpuacct/cpuacct.usage)
+tstart=$(date +%s%N) #nanoseconds since unix epoch timestamp
+cstart=$(cat /sys/fs/cgroup/cpuacct/cpuacct.usage) #nanoseconds of cpu usage at tstart 
 
 sleep 3 #sample interval
 
 tstop=$(date +%s%N)
-cstop=$(cat /sys/fs/cgroup/cpuacct/cpuacct.usage)
+cstop=$(cat /sys/fs/cgroup/cpuacct/cpuacct.usage) #nanoseoncs of cpu usage at tstop
 
 calc() { awk "BEGIN{printf \"%.2f\", $*}"; }
 cpu=$(calc "($cstop - $cstart) / ($tstop - $tstart) * 100")
