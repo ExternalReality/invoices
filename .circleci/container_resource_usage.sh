a44f6e4857cd8684b661a522c4714125fb75661c
externalreality <ejones@protonmail.com>
2020-06-03 14:25:20 -0400

    Increase resource class size.

--- .circleci/container_resource_usage.sh
+++ .circleci/container_resource_usage.sh
@@ -2,17 +2,21 @@
 
 set -e
 
-tstart=$(date +%s%N) #nanoseconds since unix epoch timestamp
-cstart=$(cat /sys/fs/cgroup/cpuacct/cpuacct.usage) #nanoseconds of cpu usage at tstart 
+ncpus=$(nproc)
+while true; do
+    tstart=$(date +%s%N)                               #nanoseconds since unix epoch timestamp
+    cstart=$(cat /sys/fs/cgroup/cpuacct/cpuacct.usage) #nanoseconds of cpu usage at tstart
 
-sleep 3 #sample interval
+    sleep 5 #sample interval
 
-tstop=$(date +%s%N)
-cstop=$(cat /sys/fs/cgroup/cpuacct/cpuacct.usage) #nanoseoncs of cpu usage at tstop
+    tstop=$(date +%s%N)
+    cstop=$(cat /sys/fs/cgroup/cpuacct/cpuacct.usage) #nanoseoncs of cpu usage at tstop
 
-calc() { awk "BEGIN{printf \"%.2f\", $*}"; }
-cpu=$(calc "($cstop - $cstart) / ($tstop - $tstart) * 100")
-mem=$(cat /sys/fs/cgroup/memory/memory.usage_in_bytes  | awk '{ byte =$1 /1024/1024; print byte "m" }')
-now=$(date +%s)
+    calc() { awk "BEGIN{printf \"%.2f\", $*}"; }
+    cpu=$(calc "($cstop - $cstart) / (($tstop - $tstart) * $ncpus) * 100")
+    mem=$(cat /sys/fs/cgroup/memory/memory.usage_in_bytes  | awk '{ byte=$1 /1024/1024; print byte "m" }')
+    now=$(date +%s)
 
-echo $now $mem $cpu
+    echo $now $mem $cpu
+done >> usage.dat
+
